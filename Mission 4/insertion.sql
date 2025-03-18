22c1753f40959223a21982b245d9a6fc09660f91
Scorpionfurax <messidandou1@gmail.com>
2025-03-18 11:50:39 +0100

    Update insertion.sql

--- Mission 4/insertion.sql
+++ Mission 4/insertion.sql
@@ -3,7 +3,7 @@ delete From facture;
 delete From ligue_facture;
 delete From ligue;
 
-INSERT INTO PRESTATION (reference, libelle, pu) VALUES
+INSERT INTO prestation (reference, libelle, pu) VALUES
 ('AFFRAN', 'Affranchissement', 3.33),
 ('PHOTOCOULEUR', 'Photocopies couleur', 0.24),
 ('PHOTON&B', 'Photocopies Noir et Blanc', 0.055);
