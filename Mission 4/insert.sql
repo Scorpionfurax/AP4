d6fc131e920f285351eae9947a55be9caacd78ea
yanissml4110 <144224591+yanissml4110@users.noreply.github.com>
2025-03-19 13:57:53 +0100

    insert.sql

---
+++ Mission 4/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO LIGUE (code_client, intitule, tresorier, spe) VALUES
+(411007, 'Ligue Lorraine Escrime', 'Valérie LAHEURTE', 'Basket'),
+(411008, 'Ligue Lorraine de FootBall', 'Pierre LENOIR', 'Escrime'),
+(411009, 'Ligue Lorraine de Basket', 'Mohamed BOURGARD', 'Foot')
