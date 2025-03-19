4dd2bb8bc73146f7da673ecf816ff6090b19a097
yanissml4110 <144224591+yanissml4110@users.noreply.github.com>
2025-03-19 13:58:46 +0100

    correction

--- Mission 4/insertion.sql
+++ Mission 4/insertion.sql
@@ -7,3 +7,8 @@ INSERT INTO prestation (reference, libelle, pu) VALUES
 ('AFFRAN', 'Affranchissement', 3.33),
 ('PHOTOCOULEUR', 'Photocopies couleur', 0.24),
 ('PHOTON&B', 'Photocopies Noir et Blanc', 0.055);
+
+INSERT INTO LIGUE (code_client, intitule, tresorier, spe) VALUES
+(411007, 'Ligue Lorraine Escrime', 'Valérie LAHEURTE', 'Basket'),
+(411008, 'Ligue Lorraine de FootBall', 'Pierre LENOIR', 'Escrime'),
+(411009, 'Ligue Lorraine de Basket', 'Mohamed BOURGARD', 'Foot')
