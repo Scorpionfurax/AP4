631b52a32cffa10eed733a6f10d543698d371a8d
Scorpionfurax <messidandou1@gmail.com>
2025-03-20 12:25:31 +0100

    Update insertion.sql

--- Mission 4/insertion.sql
+++ Mission 4/insertion.sql
@@ -8,7 +8,7 @@ INSERT INTO prestation (reference, libelle, pu) VALUES
 ('PHOTOCOULEUR', 'Photocopies couleur', 0.24),
 ('PHOTON&B', 'Photocopies Noir et Blanc', 0.055);
 
-INSERT INTO LIGUE (code_client, intitule, tresorier, spe) VALUES
+INSERT INTO ligue (code_client, intitule, tresorier, spe) VALUES
 (411007, 'Ligue Lorraine Escrime', 'Valérie LAHEURTE', 'Basket'),
 (411008, 'Ligue Lorraine de FootBall', 'Pierre LENOIR', 'Escrime'),
 (411009, 'Ligue Lorraine de Basket', 'Mohamed BOURGARD', 'Foot')
